323d50f0210c77a1c01fbf12f7a77e1c22f69040
soycode <soycode@users.noreply.github.com>
2018-05-08 11:07:43 -0700

    Create initial Bookmark model

--- djorg/settings.py
+++ djorg/settings.py
@@ -31,12 +31,16 @@ ALLOWED_HOSTS = ['0.0.0.0']
 # Application definition
 
 INSTALLED_APPS = [
+    # Django-included apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # 3rd party apps
+    # Our apps
+    'bookmarks',
 ]
 
 MIDDLEWARE = [
